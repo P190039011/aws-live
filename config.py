fa610a1cbf4b6458554d210290308be3c0fa57fa
P190039011 <87218530+P190039011@users.noreply.github.com>
2022-04-18 14:41:54 +0530

    Add files via upload
    commit 1

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cmxnkilgtv5f.us-east-2.rds.amazonaws.com"
+customuser = "kishore"
+custompass = "kishoresonti"
+customdb = "employee"
+custombucket = "ademployee"
+customregion = "us-east-2b"
+
